49c0e05f0f201ff0e946bff1f120ff15a4d267f7
Bruno Adam Osiek <baosiek@gmail.com>
2020-12-03 10:39:57 -0800

    TCPJMXServer documented

--- tcpserver/src/main/java/ca/baosiek/server/TCPJMXServer.java
+++ tcpserver/src/main/java/ca/baosiek/server/TCPJMXServer.java
@@ -5,7 +5,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.management.*;
-import java.io.*;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.lang.management.ManagementFactory;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -25,10 +28,11 @@ public class TCPJMXServer implements Runnable, TCPJMXServerMXBean {
 
     /**
      * Returns a TCPJMXServer object that can then be monitored and managed via JMX.
+     * Basically this objects acts on socket connections.
      *
-     * @param   port  an absolute URL giving the base location of the image
-     * @param   serverName the location of the image, relative to the url argument
-     * @see     TCPJMXServer
+     * @param port       an absolute URL giving the base location of the image
+     * @param serverName the location of the image, relative to the url argument
+     * @see TCPJMXServer
      */
 
     public TCPJMXServer(String serverName, int port) {
@@ -123,6 +127,9 @@ public class TCPJMXServer implements Runnable, TCPJMXServerMXBean {
         LOG.info(String.format("Server %s started running at port: %d at %s", this.serverName, this.port.get(),
                 LocalDateTime.now()));
 
+        /*
+        This loops keep being executed until this server is stopped.
+         */
         while (!this.isStopped) {
 
             try {
@@ -136,6 +143,9 @@ public class TCPJMXServer implements Runnable, TCPJMXServerMXBean {
 
                 if (!this.isSuspended) {
 
+                    /*
+                    Handles socket connections in a new thread
+                     */
                     if (socket != null) {
                         new TCPJMXClientHandler(socket.accept()).start();
                     }
@@ -143,10 +153,14 @@ public class TCPJMXServer implements Runnable, TCPJMXServerMXBean {
 
                 socket.close();
 
-                while(this.isSuspended) {
+                /*
+                This loops keep being executed until a notify()
+                from resume() is executed.
+                */
+                while (this.isSuspended) {
 
                     LOG.info(String.format("Server [%s] suspended at [%s]", this.serverName, LocalDateTime.now()));
-                    synchronized(this) {
+                    synchronized (this) {
                         try {
                             wait();
                         } catch (InterruptedException e) {
@@ -157,18 +171,29 @@ public class TCPJMXServer implements Runnable, TCPJMXServerMXBean {
 
                     LOG.info(String.format("Server [%s] resumed at [%s]", this.serverName, LocalDateTime.now()));
                 }
-                
+
             } catch (IOException e) {
 
-                if (e.getLocalizedMessage().equals("Socket closed")) continue;
-				LOG.error(e.getLocalizedMessage());
+                /*
+                This IOException is expected if these flags are raised.
+                I further check the message "Socket closed" just to be
+                in the safe side. Should this message change in the future
+                the IOException will be triggered and easily updated should
+                this be the case.
+                 */
+                if ((this.isStopped || this.isSuspended) &&
+                        e.getLocalizedMessage().equals("Socket closed")) {
+                    continue;
+                }
+
+                e.printStackTrace();
             }
         }
         LOG.info(String.format("Server [%s] stopped at [%s]", this.serverName, LocalDateTime.now()));
     }
 
     /**
-     *  This handler in different threads enables handling of multiple requests
+     *  This handler being executed in different threads enables handling of multiple requests.
       */
 
     private class TCPJMXClientHandler extends Thread {
@@ -180,13 +205,13 @@ public class TCPJMXServer implements Runnable, TCPJMXServerMXBean {
         }
 
         /**
-         * Just handling the ping, by replying with the string "pong!".
+         * Just treating the ping, by replying with the string "pong!".
          */
         public void run() {
 
             try {
-                LOG.info(String.format("New client socket accepted at channel %s at %s", this.clientSocket.getInetAddress(),
-                        LocalDateTime.now()));
+                LOG.info(String.format("New client socket accepted at channel %s at %s",
+                        this.clientSocket.getInetAddress(), LocalDateTime.now()));
 
                 OutputStream out = clientSocket.getOutputStream();
                 BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(out));
@@ -202,6 +227,10 @@ public class TCPJMXServer implements Runnable, TCPJMXServerMXBean {
         }
     }
 
+    /**
+     * This main method is intended to facilitate debugging. Otherwise
+     * use {@code TCPJMXServerExecutor}
+     */
     public static void main(String[] args) throws MalformedObjectNameException,
             NotCompliantMBeanException, InstanceAlreadyExistsException, MBeanRegistrationException {
 
--- tcpserver/src/main/java/ca/baosiek/server/TCPJMXServerExecutor.java
+++ tcpserver/src/main/java/ca/baosiek/server/TCPJMXServerExecutor.java
@@ -1,10 +1,12 @@
 package ca.baosiek.server;
 
+import org.apache.commons.cli.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.management.*;
 import java.lang.management.ManagementFactory;
+import java.security.InvalidParameterException;
 import java.time.LocalDateTime;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -14,7 +16,7 @@ public class TCPJMXServerExecutor {
     private final ExecutorService serversExecutor; // Executor service to manage servers threads
     private static Logger LOG = LoggerFactory.getLogger(TCPJMXServerExecutor.class);
 
-    public TCPJMXServerExecutor() {
+    public TCPJMXServerExecutor(int threads) {
 
         serversExecutor = Executors.newFixedThreadPool(10);
     }
@@ -36,11 +38,76 @@ public class TCPJMXServerExecutor {
         }
     }
 
+    /**
+     * Initialize ExecuterService with the number of thread computed from
+     * blocking coefficient.
+     * @param args should contain -n for number of servers; -p for starting port; and
+     *             -c for blocking coefficient
+     */
+
     public static void main(String[] args) {
 
-        int noServers = Integer.parseInt(args[0]);
-        int portSeqStart = Integer.parseInt(args[1]);
-        TCPJMXServerExecutor serversExecutor = new TCPJMXServerExecutor();
+        int noServers = 0;
+        int portSeqStart = 0;
+        float blockingCoefficient = 0f;
+
+        // Setting the options
+        Options options = new Options();
+        options.addOption("n", true, "number of servers");
+        options.addOption("p", true, "starting port");
+        options.addOption("c", true, "blocking coefficient");
+
+        // Parsing command line
+        CommandLineParser parser = new DefaultParser();
+        CommandLine cmd = null;
+        try {
+            cmd = parser.parse(options, args);
+
+           /*
+            Checking and getting number of servers to be initialized.
+             */
+            if (cmd.hasOption('n')) {
+                noServers = Integer.parseInt(cmd.getOptionValue('n'));
+            } else {
+                throw new InvalidParameterException("Number of servers wasn't specified");
+            }
+
+             /*
+            Checking and getting the initial port.
+             */
+            if (cmd.hasOption('p')) {
+                portSeqStart = Integer.parseInt(cmd.getOptionValue('p'));
+            } else {
+                throw new InvalidParameterException("Starting port wasn't specified");
+            }
+
+             /*
+            Checking and getting the blocking coefficient. Number must be greater or equal to 0
+            and smaller than 1.
+             */
+            if (cmd.hasOption('c')) {
+                blockingCoefficient = Float.parseFloat(cmd.getOptionValue('c'));
+                if (blockingCoefficient < 0 || blockingCoefficient >= 1){
+                    throw new InvalidParameterException(
+                            String.format("Blocking coefficient is a number in the interval [0,1)." +
+                            " %.2f was informed.", blockingCoefficient));
+                }
+            } else {
+                throw new InvalidParameterException("Blocking coefficient wasn't specified");
+            }
+
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        /*
+        Calculating optimal number of threads based on Subramaniam (2011, p.31)
+         */
+        int cores = Runtime.getRuntime().availableProcessors();
+        int threads = (int) (cores / (1 - blockingCoefficient));
+        LOG.info(String.format("Setting number of threads to %d", threads));
+
+        TCPJMXServerExecutor serversExecutor = new TCPJMXServerExecutor(threads);
         try {
             serversExecutor.startServers(noServers, portSeqStart);
 
